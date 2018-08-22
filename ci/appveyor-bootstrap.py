f95e3a812cd251f0e88e508cb03e88820a419ed3
Joseph Benden <joe@benden.us>
2018-08-21 19:51:35 -0700

    Added Python 3.7 support.

--- ci/appveyor-bootstrap.py
+++ ci/appveyor-bootstrap.py
@@ -26,6 +26,8 @@ URLS = {
     ("3.5", "32"): BASE_URL + "3.5.4/python-3.5.4.exe",
     ("3.6", "64"): BASE_URL + "3.6.2/python-3.6.2-amd64.exe",
     ("3.6", "32"): BASE_URL + "3.6.2/python-3.6.2.exe",
+    ("3.7", "64"): BASE_URL + "3.7.0/python-3.7.0-amd64.exe",
+    ("3.7", "32"): BASE_URL + "3.7.0/python-3.7.0.exe",
 }
 INSTALL_CMD = {
     # Commands are allowed to fail only if they are not the last command.  Eg: uninstall (/x) allowed to fail.
@@ -35,6 +37,7 @@ INSTALL_CMD = {
             ["msiexec.exe", "/L*+!", "install.log", "/qn", "/i", "{path}", "TARGETDIR={home}"]],
     "3.5": [["{path}", "/quiet", "TargetDir={home}"]],
     "3.6": [["{path}", "/quiet", "TargetDir={home}"]],
+    "3.7": [["{path}", "/quiet", "TargetDir={home}"]],
 }
 
 
