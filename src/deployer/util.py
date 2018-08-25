6c51744572c950979a0c07ec47115ff0ae80b489
Joseph Benden <joe@benden.us>
2018-08-25 14:32:03 -0700

    Added module providing utility functionality.

---
+++ src/deployer/util.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joseph Benden <joe@benden.us>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+A module providing support functionality.
+
+.. moduleauthor:: Joseph Benden <joe@benden.us>
+
+:copyright: (c) Copyright 2018 by Joseph Benden.
+:license: Apache License 2.0, see LICENSE.txt for full details.
+"""
+
+
+def merge_dicts(*dict_args):
+    """Merge dictionaries.
+
+    Given any number of dicts, shallow copy and merge into a new dict.
+
+    Precedence goes to key value pairs in latter dicts.
+    """
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
