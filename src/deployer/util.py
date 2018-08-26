4ce1e6daab26eff308f6386ddf2ef4512fbafcea
Joseph Benden <joe@benden.us>
2018-08-26 11:58:21 -0700

    Added initial process spawn.

--- src/deployer/util.py
+++ src/deployer/util.py
@@ -22,6 +22,26 @@ A module providing support functionality.
 :license: Apache License 2.0, see LICENSE.txt for full details.
 """
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import errno
+import logging
+import sys
+from io import BytesIO
+
+from twisted.internet import defer
+from twisted.internet.defer import Deferred
+from twisted.internet.error import ProcessDone
+from twisted.internet.error import ProcessTerminated
+from twisted.internet.error import ReactorNotRunning
+from twisted.internet.protocol import ProcessProtocol
+from twisted.python.failure import Failure
+
+IS_WINDOWS = sys.platform.lower().startswith('win')
+LOGGER = logging.getLogger(__name__)
+
 
 def merge_dicts(*dict_args):
     """Merge dictionaries.
@@ -34,3 +54,110 @@ def merge_dicts(*dict_args):
     for dictionary in dict_args:
         result.update(dictionary)
     return result
+
+
+class SubprocessProtocol(ProcessProtocol):
+    """Simple Twisted protocol which captures a process'es standard output."""
+
+    def connectionMade(self):
+        """Triggered upon program start, as it is entering its' `main` function."""
+        self.d = Deferred()
+        self.outBuf = BytesIO()
+        self.errBuf = BytesIO()
+        self.outReceived = self.outBuf.write
+        self.errReceived = self.errBuf.write
+
+    def processEnded(self, reason):
+        """Triggered upon the end of a program's execution."""
+        out = self.outBuf.getvalue()
+        err = self.errBuf.getvalue()
+        if reason.check(ProcessDone):
+            self.d.callback(out)
+        else:
+            LOGGER.error(err)
+            self.d.errback(reason)
+
+
+def async_check_output(args, reactor_process=None):
+    """Execute and capture a process'es standard output, using ```Twisted```.
+
+    :type args: list of str
+    :type reactor_process: :class: twisted.internet.interfaces.IReactorProcess
+    :rtype: Deferred
+    """
+    if reactor_process is None:                # noqa: no-cover
+        from twisted.internet import reactor   # noqa: no-cover
+        reactor_process = reactor              # noqa: no-cover
+
+    process_protocol = SubprocessProtocol()
+    try:
+        reactor_process.spawnProcess(process_protocol, args[0], args)
+        return process_protocol.d
+    except OSError as e:
+        if e.errno is None or e.errno == errno.ENOENT:
+            return defer.fail(ProcessTerminated(exitCode=1))
+        else:
+            return defer.fail(e)
+
+
+def sync_check_output(argv=()):
+    """Execute and capture a process'es standard output."""
+    import threading
+
+    event = threading.Event()
+    output = [None]
+
+    def _cb(result):
+        output[0] = result.decode('utf-8').rstrip("\r\n")
+        event.set()
+
+    def _cbe(result):
+        output[0] = result
+        event.set()
+
+    d = async_check_output(argv)
+    d.addCallback(_cb)
+    d.addErrback(_cbe)
+
+    event.wait(None)
+
+    if isinstance(output[0], Failure):
+        output[0].raiseException()
+
+    return output[0]
+
+
+def start_reactor():
+    """Start a ```Twisted``` reactor, in a background thread."""
+    from twisted.internet import reactor as _reactor
+    import threading
+
+    event = threading.Event()
+
+    def _start():
+        event.set()
+        _reactor.run(installSignalHandlers=0)
+
+    import threading
+    t = threading.Thread(target=_start, group=None)
+    t.daemon = True
+    t.start()
+
+    event.wait(None)
+
+    if not IS_WINDOWS:              # noqa: no-cover
+        _reactor._handleSignals()   # noqa: no-cover
+
+    return _reactor
+
+
+def stop_reactor(the_reactor):
+    """Stop a running ```Twisted``` reactor."""
+    def stop(result, stopReactor):
+        if stopReactor:                # noqa: no-cover
+            try:                       # noqa: no-cover
+                the_reactor.stop()     # noqa: no-cover
+            except ReactorNotRunning:  # noqa: no-cover
+                pass                   # noqa: no-cover
+
+    the_reactor.callWhenRunning(stop, True, True)  # noqa: no-cover
---
+++ tests/test_util.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joseph Benden <joe@benden.us>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+import pytest
+from hamcrest import assert_that
+from hamcrest import calling
+from hamcrest import equal_to
+from hamcrest import raises
+from twisted.internet.error import ProcessTerminated
+
+from deployer.util import start_reactor
+from deployer.util import stop_reactor
+from deployer.util import sync_check_output
+
+IS_WINDOWS = sys.platform.lower().startswith('win')
+
+
+@pytest.fixture(scope="module")
+def reactor(request):
+    r = start_reactor()
+    yield
+    stop_reactor(r)
+
+
+@pytest.mark.skipif(IS_WINDOWS, reason='Irrelevant on non-unix')
+def test_util_unix_echo_works(reactor):
+    subject = sync_check_output(['/bin/echo', 'hello world'])
+    assert_that(subject, equal_to("hello world"))
+
+
+@pytest.mark.skipif(IS_WINDOWS, reason='Irrelevant on non-unix')
+def test_util_unix_nonexisting_fails(reactor):
+    assert_that(calling(sync_check_output).with_args(['/nonexistent', 'hello world']), raises(ProcessTerminated))
+
+
+@pytest.mark.skipif(not IS_WINDOWS, reason='Irrelevant on unix')
+def test_util_win_echo_works(reactor):
+    assert_that(sync_check_output(["\\Windows\\System32\\cmd.exe", "/c", "echo", "hello", "world"]), equal_to("hello world"))  # noqa: no-cover
+
+
+@pytest.mark.skipif(not IS_WINDOWS, reason='Irrelevant on unix')
+def test_util_win_nonexisting_fails(reactor):
+    assert_that(calling(sync_check_output).with_args(["sdfsdfdsf329909092"]), raises(ProcessTerminated))
